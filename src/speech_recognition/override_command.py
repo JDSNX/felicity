118cadb486747af7a9399c3fa9bd6e715329d485
JDSNX <judisonbacalso24@gmail.com>
2023-08-25 07:25:57 +0800

    change errors in override_command

--- src/speech_recognition/override_command.py
+++ src/speech_recognition/override_command.py
@@ -1,11 +1,11 @@
 import RPi.GPIO as GPIO
 from gpiozero import Servo
-from time import time, sleep
+from time import sleep
 from backend.config import (
   settings, logger
 )
-from schema.schemas import Execute
  
+
 def update_sql(app, execute, isOpen):
     """
     To be change to SQLAlchemy
@@ -62,53 +62,53 @@ if __name__ == '__main__':
     set_gpio()
 
     while True:
-    """
-    WIP
-    Code will be change according to SQLAlchemy
-    """
-    # conn = pyodbc.connect('DRIVER=FreeTDS;'
-    # 'SERVER=192.168.1.13;'
-    # 'PORT=1433;'
-    # 'DATABASE=EPMAtry;'
-    # 'UID=epma;'
-    # 327 | Page
-    # 'PWD=epma;'
-    # 'TDS_Version=8.0;')
-    # cursor = conn.cursor()
-    # cursor.execute('''
-    # SELECT light, window, door, isExecuteDoor, isExecuteLight, isExecuteWindow
-    # FROM dbo.ROOMS
-    # WHERE PatientID = ? AND RoomNo = ?''',(PatientID, PatientRoom))
-    
-    # for row in cursor:
-    #   isLights = row.isExecuteLight
-    #   isDoor = row.isExecuteDoor
-    #   isWindow = row.isExecuteWindow
-    
-    # if isLights: 
-    #   if Execute.LIGHT:
-    #     execute_gpio(PIN_LIGHT, True)
-    #     update_sql('light', 'isExecuteLight', 0)
-    #     Execute.LIGHT = False
-    #   else:
-    #     execute_gpio(PIN_LIGHT, False)
-    #     update_sql('light', 'isExecuteLight', 1)
-    #     Execute.LIGHT = True
-    # elif isDoor:
-    #   if Execute.DOOR:
-    #     execute_gpio(PIN_DOOR, True)
-    #     update_sql('door', 'isExecuteDoor', 0)
-    #     Execute.DOOR = False
-    #   else:
-    #     execute_gpio(PIN_DOOR, False)
-    #     update_sql('door', 'isExecuteDoor', 1)
-    #     Execute.DOOR = True
-    # elif isWindow:
-    #   if Execute.WINDOW:
-    #     update_sql('window', 'isExecuteWindow', 0)
-    #     ServoCycle(False)
-    #     Execute.WINDOW = False
-    #   else:
-    #     update_sql('window', 'isExecuteWindow', 1)
-    #     ServoCycle(True)
-    #     Execute.WINDOW = True
+        """
+        WIP
+        Code will be change according to SQLAlchemy
+        """
+        # conn = pyodbc.connect('DRIVER=FreeTDS;'
+        # 'SERVER=192.168.1.13;'
+        # 'PORT=1433;'
+        # 'DATABASE=EPMAtry;'
+        # 'UID=epma;'
+        # 327 | Page
+        # 'PWD=epma;'
+        # 'TDS_Version=8.0;')
+        # cursor = conn.cursor()
+        # cursor.execute('''
+        # SELECT light, window, door, isExecuteDoor, isExecuteLight, isExecuteWindow
+        # FROM dbo.ROOMS
+        # WHERE PatientID = ? AND RoomNo = ?''',(PatientID, PatientRoom))
+        
+        # for row in cursor:
+        #   isLights = row.isExecuteLight
+        #   isDoor = row.isExecuteDoor
+        #   isWindow = row.isExecuteWindow
+        
+        # if isLights: 
+        #   if settings.light:
+        #     execute_gpio(PIN_LIGHT, True)
+        #     update_sql('light', 'isExecuteLight', 0)
+        #     settings.light = False
+        #   else:
+        #     execute_gpio(PIN_LIGHT, False)
+        #     update_sql('light', 'isExecuteLight', 1)
+        #     settings.light = True
+        # elif isDoor:
+        #   if settings.door:
+        #     execute_gpio(PIN_DOOR, True)
+        #     update_sql('door', 'isExecuteDoor', 0)
+        #     settings.door = False
+        #   else:
+        #     execute_gpio(PIN_DOOR, False)
+        #     update_sql('door', 'isExecuteDoor', 1)
+        #     settings.door = True
+        # elif isWindow:
+        #   if settings.window:
+        #     update_sql('window', 'isExecuteWindow', 0)
+        #     ServoCycle(False)
+        #     settings.window = False
+        #   else:
+        #     update_sql('window', 'isExecuteWindow', 1)
+        #     ServoCycle(True)
+        #     settings.window = True
