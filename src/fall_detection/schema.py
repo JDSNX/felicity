bbb681ddd2048c5a1757b89445923d64723ae049
JDSNX <judisonbacalso24@gmail.com>
2023-09-04 03:13:08 +0800

    fix bug enum

--- src/fall_detection/schema.py
+++ src/fall_detection/schema.py
@@ -1,5 +1,5 @@
 from enum import Enum
 
 class Location(str, Enum):
-    LIVING_ROOM: "living_room"
-    COMFORT_ROOM: "comfort_room"
+    LIVING_ROOM = "living_room"
+    COMFORT_ROOM = "comfort_room"
