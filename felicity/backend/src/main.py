20eb6386900ae9ea29cbf8c6f2943832c62886a6
JDSNX <judisonbacalso24@gmail.com>
2023-09-05 11:07:49 +0800

    point two router

--- felicity/backend/src/main.py
+++ felicity/backend/src/main.py
@@ -1,18 +1,18 @@
 from fastapi import FastAPI
-from database.core import engine, Base
-from routers.accounts import router as accounts_router
-from routers.auth import router as auth_router
-from routers.patients import router as patients_router
+from database.core import Base
+from database.session import engine
+
+from auth.router import router as auth_router
+from users.router import router as users_router
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-app.include_router(accounts_router)
 app.include_router(auth_router)
-app.include_router(patients_router)
+app.include_router(users_router)
 
 
-@app.get("/api")
+@app.get("/")
 async def root():
     return {"message": "hello"}
