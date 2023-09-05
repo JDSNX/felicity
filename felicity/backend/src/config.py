2bc0f9d3881021f77b65f0b85a8f7eb874db46f1
JDSNX <judisonbacalso24@gmail.com>
2023-09-05 09:53:05 +0800

    separate session from core

--- felicity/backend/src/config.py
+++ felicity/backend/src/config.py
@@ -12,7 +12,7 @@ class Settings(BaseSettings):
     secret_key: str
     access_token_expire_minutes: int
 
-    model_config = SettingsConfigDict(env_file=".env")
+    model_config = SettingsConfigDict(env_file="../.env")
 
 
 settings = Settings()
--- felicity/backend/src/database/core.py
+++ felicity/backend/src/database/core.py
@@ -1,11 +1,5 @@
-from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from config import settings
-
-engine = create_engine(settings.database_url, connect_args={"check_same_thread": False})
-
-session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+from .session import session_local
 
 Base = declarative_base()
 
