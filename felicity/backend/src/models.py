082455f8ca72af0c7f80d272d0fb1f55e7a648df
JDSNX <judisonbacalso24@gmail.com>
2023-09-07 08:11:17 +0800

    Add serializable_dict to CustomModel

--- felicity/backend/src/models.py
+++ felicity/backend/src/models.py
@@ -1,60 +1,37 @@
-from database.core import Base
 from datetime import datetime
-from passlib import hash
-from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey
+from typing import Any
+from zoneinfo import ZoneInfo
+
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel, ConfigDict, model_validator
 
 
-class Account(Base):
-    __tablename__ = "accounts"
+def convert_datetime_to_gmt(dt: datetime) -> str:
+    if not dt.tzinfo:
+        dt = dt.replace(tzinfo=ZoneInfo("UTC"))
 
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, nullable=False)
-    password = Column(String, nullable=False)
-    first_name = Column(String, nullable=False)
-    middle_name = Column(String, nullable=False)
-    last_name = Column(String, nullable=False)
-    contact_number = Column(String, nullable=False)
-    is_admin = Column(Boolean, nullable=False)
-
-    created_at = Column(DateTime, default=datetime.utcnow)
-    last_updated = Column(DateTime, default=datetime.utcnow)
-
-    def verify_password(self, password: str = None):
-        return hash.bcrypt.verify(password, self.password)
+    return dt.strftime("%Y-%m-%dT%H:%M:%S%z")
 
 
-class Patient(Base):
-    __tablename__ = "patients"
+class CustomModel(BaseModel):
+    model_config = ConfigDict(
+        json_encoders={datetime: convert_datetime_to_gmt},
+        populate_by_name=True,
+    )
 
-    id = Column(Integer, primary_key=True, index=True)
-    room_no = Column(String, nullable=False)
-    first_name = Column(String, nullable=False)
-    middle_name = Column(String, nullable=False)
-    last_name = Column(String, nullable=False)
-    address = Column(String, nullable=False)
-    place_of_birth = Column(String, nullable=False)
-    gender = Column(String, nullable=False)
-    contact_person = Column(String, nullable=False)
-    contact_number = Column(String, nullable=False)
-    date_of_birth = Column(DateTime, nullable=False)
-    is_connected = Column(Boolean, nullable=False, default=False)
-    is_fall = Column(Boolean, nullable=False, default=False)
+    @model_validator(mode="before")
+    @classmethod
+    def set_null_microseconds(cls, data: dict[str, Any]) -> dict[str, Any]:
+        datetime_fields = {
+            k: v.replace(microsecond=0)
+            for k, v in data.items()
+            if isinstance(k, datetime)
+        }
 
-    created_at = Column(DateTime, default=datetime.utcnow)
-    last_updated = Column(DateTime, default=datetime.utcnow)
+        return {**data, **datetime_fields}
 
+    def serializable_dict(self, **kwargs):
+        """Return a dict which contains only serializable fields."""
+        default_dict = self.model_dump()
 
-class Fall(Base):
-    __tablename__ = "falls"
-
-    id = Column(Integer, primary_key=True, index=True)
-    patient_id = Column(Integer, ForeignKey("patients.id"))
-    date_of_fall = Column(DateTime, index=True)
-
-
-class Room(Base):
-    __tablename__ = "rooms"
-
-    id = Column(Integer, primary_key=True, index=True)
-    patient_id = Column(Integer, ForeignKey("patients.id"))
-    room_number = Column(String, index=True)
+        return jsonable_encoder(default_dict)
